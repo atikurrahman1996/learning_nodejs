f229cb12bbd1e473fce0e1b26c835970588d00ac
atik <atikur2013rahman@gmail.com>
2023-07-25 10:55:12 +0600

    regular expression

--- RegularExpression/index.js
+++ RegularExpression/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
 
+// will print single digit
+
 app.get("/products/:id([0-9])", (req, res) => {
   res.send(`ID = ${req.params.id}`);
 });
+
+// will print combination of 3 digit
+
+app.get("/products/:id([0-9]{3})", (req, res) => {
+  res.send(`ID = ${req.params.id}`);
+});
+
+// title would be 5 chracter
+
 app.get("/products/:title([a-z]{5})", (req, res) => {
   res.send(`title = ${req.params.title}`);
 });
