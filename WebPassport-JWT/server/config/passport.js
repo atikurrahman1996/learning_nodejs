e6b98c1642945f9933c305744a17186b81c1f1cb
atik <atikur2013rahman@gmail.com>
2023-08-01 14:08:14 +0600

    added jwt

--- WebPassport-JWT/server/config/passport.js
+++ WebPassport-JWT/server/config/passport.js
@@ -8,16 +8,16 @@ const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.SECRET_KEY;
 passport.use(
-  new JwtStrategy(opts, function (jwt_payload, done) {
-    User.findOne({ id: jwt_payload.id }, (err, user) => {
-      if (err) {
-        return done(err, false);
-      }
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    try {
+      const user = await User.findOne({ _id: jwt_payload.id });
       if (user) {
         return done(null, user);
       } else {
         return done(null, false);
       }
-    });
+    } catch (err) {
+      return done(err, false);
+    }
   })
 );
